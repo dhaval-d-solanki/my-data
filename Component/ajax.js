6fa9645e2b9749b2da638e47ec3fd2b0cb101bd2
dhaval-d-solanki <dhaval.d.solanki@gmail.com>
2017-03-29 10:38:54 +0530

    add ajax component

---
+++ Component/ajax.js
@@ -0,0 +1,48 @@
+/*Author: Dhaval Solanki AJAX related code starts */
+ajaxTrim = function(obj){
+ $.each( obj, function( key, value ) {
+   obj[key] = $.trim(value);
+ });
+}
+ajaxSendRequest = function (url, orgCallback, callback, parameters, methodType, isAsync) {
+	parameters = parameters || {};
+	console.log(parameters);
+	$.ajax({
+		url : url,
+		timeout : 600000,
+		async : isAsync,
+		cache : false,
+		type : methodType,
+		data : parameters,
+		dataType : "json",
+		success : function (response) {
+			orgCallback.callback = typeof orgCallback.callback == 'function' ? orgCallback.callback : $.noop;
+			callback(response, orgCallback);
+		},
+		error : function (jqXHR, textStatus, errorThrown) {
+			if (jqXHR.status === 0 || jqXHR.readyState === 0 || jqXHR.statusText == "abort") {
+				return;
+			}
+			// here you can show system error popup
+		}
+	});
+};
+
+ajaxService = function (url, callbackObject, parameters, methodType, isAsync) {
+	/*Trim all parameter values*/
+	ajaxTrim(parameters);
+	ajaxSendRequest(url, callbackObject, ajaxServiceCallback, parameters, methodType, isAsync);
+};
+ajaxServiceCallback = function (responseObjData, callbackObject) {
+	if (responseObjData != null) {
+		//if(responseObjData.SessionExpired){} to handle session expired scenario
+		callbackObject.callback.call(callbackObject.context, responseObjData);
+	}
+};
+
+ajaxRequest = function (url, postDataObj, _callback, methodType ) {
+	var methodType = methodType === "GET" ? methodType : "POST";
+	//url, orgCallback, callback, parameters, methodType, isAsync
+	ajaxService(url, { callback : _callback, context : this }, postDataObj, methodType, true);	
+}
+/* AJAX related code ends */
